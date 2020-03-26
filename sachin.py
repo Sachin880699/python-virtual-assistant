44b0477c920f296020adb0dcd253cfa6e88c40ed
Sachin880699 <sachinnihcas373@gmail.com>
2020-03-26 17:33:51 +0530

    virtual assistance project

---
+++ sachin.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr  
+from time import sleep
+# get audio from the microphone                                                                       
+r = sr.Recognizer()
+
+def main():
+    with sr.Microphone() as source:
+        sleep(1)
+        print("Say something and wait wait.")
+        audio = r.listen(source)
+        sleep(1)
+
+    try:
+        data = r.recognize_google(audio)
+        print(data)
+        
+        
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+        
+        
+main()
